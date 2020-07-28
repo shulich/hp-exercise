b89b3983338288cae73ba3022b0460dcf743047e
shulich <48982660+shulich@users.noreply.github.com>
2020-07-28 23:29:15 +0300

    Update instelation.component.ts

--- src/app/components/instelation/instelation.component.ts
+++ src/app/components/instelation/instelation.component.ts
@@ -1,1 +1,16 @@
 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-instelation',
+  templateUrl: './instelation.component.html',
+  styleUrls: ['./instelation.component.scss']
+})
+export class InstelationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
