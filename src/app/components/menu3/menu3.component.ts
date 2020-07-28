9c4ea7e4ecc2554454055ca01db3c199aa9e788b
shulich <48982660+shulich@users.noreply.github.com>
2020-07-28 23:29:56 +0300

    Update menu3.component.ts

--- src/app/components/menu3/menu3.component.ts
+++ src/app/components/menu3/menu3.component.ts
@@ -1,1 +1,16 @@
 
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu3',
+  templateUrl: './menu3.component.html',
+  styleUrls: ['./menu3.component.scss']
+})
+export class Menu3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
